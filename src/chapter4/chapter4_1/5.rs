f7b21a29373ab3451610803d44bae64e26a4bd6d
JDNew <chijiaduo@hotmail.com>
2022-03-19 20:49:50 +0800

    feat: chapter4_1 practice

---
+++ src/chapter4/chapter4_1/5.rs
@@ -0,0 +1,15 @@
+
+// // 解决代码中的错误和 `panic`
+// fn main() {
+//     let v1 = 251_u8 + 8;
+//     let v2 = i8::checked_add(251, 8).unwrap();
+//     println!("{},{}",v1,v2);
+// }
+
+
+// 解决代码中的错误和 `panic`
+fn main() {
+    let v1 = 251_u16 + 8;
+    let v2 = u16::checked_add(251, 8).unwrap();
+    println!("{},{}",v1,v2);
+}
