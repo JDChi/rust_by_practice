46be66218be9bcded5ad16f9b2e971fc2830a14a
JDNew <chijiaduo@hotmail.com>
2022-03-19 21:06:17 +0800

    feat: chapter4_2 practice

---
+++ src/chapter4/chapter4_2/3.rs
@@ -0,0 +1,21 @@
+
+// // make  println! work
+// fn main() {
+//     let _f: bool = false;
+//
+//     let t = true;
+//     if !t {
+//         println!("Success!")
+//     }
+// }
+
+
+// make  println! work
+fn main() {
+    let _f: bool = false;
+
+    let t = true;
+    if t {
+        println!("Success!")
+    }
+}
---
+++ src/chapter4/chapter4_2/4.rs
@@ -0,0 +1,17 @@
+
+// fn main() {
+//     let f = true;
+//     let t = true && false;
+//     assert_eq!(t, f);
+//
+//     println!("Success!")
+// }
+
+
+fn main() {
+    let f = false;
+    let t = true && false;
+    assert_eq!(t, f);
+
+    println!("Success!")
+}
