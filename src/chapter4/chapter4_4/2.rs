04d1511206aa36f1fc2d0a66fcdeced517359434
JDNew <chijiaduo@hotmail.com>
2022-03-20 11:07:34 +0800

    feat: chapter4_4 practice

---
+++ src/chapter4/chapter4_4/2.rs
@@ -0,0 +1,17 @@
+// fn main() {
+//     print();
+// }
+//
+// // 使用另一个类型来替代 i32
+// fn print() -> i32 {
+//     println!("hello,world");
+// }
+
+fn main() {
+    print();
+}
+
+// 使用另一个类型来替代 i32
+fn print() -> () {
+    println!("hello,world");
+}
---
+++ src/chapter4/chapter4_4/4.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Success!");
+}
+
+fn get_option(tp: u8) -> Option<i32> {
+    match tp {
+        1 => {
+            // TODO
+        }
+        _ => {
+            // TODO
+        }
+    };
+
+    // 这里与其返回一个 None，不如使用发散函数替代
+    never_return_fn()
+}
+
+// 使用三种方法实现以下发散函数
+// fn never_return_fn() -> ! {
+//     loop {
+//         println!("hello");
+//     }
+// }
+
+// fn never_return_fn() -> ! {
+//     panic!("never return fn");
+// }
+
+fn never_return_fn() -> ! {
+    unimplemented!();
+}
