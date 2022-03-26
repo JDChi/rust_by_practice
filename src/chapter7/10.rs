92e3f79dd7865ab7918c3bde7b1b08fe4cd15564
JDNew <chijiaduo@hotmail.com>
2022-03-26 21:08:07 +0800

    feat: chapter7

---
+++ src/chapter7/10.rs
@@ -0,0 +1,30 @@
+//
+// // 填空
+// fn main() {
+//     let mut counter = 0;
+//
+//     let result = loop {
+//         counter += 1;
+//
+//         if counter == 10 {
+//             __;
+//         }
+//     };
+//
+//     assert_eq!(result, 20);
+// }
+
+// 填空
+fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 20 {
+            break (counter);
+        }
+    };
+
+    assert_eq!(result, 20);
+}
---
+++ src/chapter7/3.rs
@@ -0,0 +1,16 @@
+// fn main() {
+//     for n in 1..=100 { // 修改此行，让代码工作
+//         if n == 100 {
+//             panic!("NEVER LET THIS RUN")
+//         }
+//     }
+// }
+
+fn main() {
+    for n in 1..100 {
+        // 修改此行，让代码工作
+        if n == 100 {
+            panic!("NEVER LET THIS RUN")
+        }
+    }
+}
